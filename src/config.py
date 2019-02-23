27074d44af4aac2ba868aae71a2207ba20fe871a
XuanC6 <95chenxuan@gmail.com>
2019-02-23 17:45:57 -0500

    update

--- src/config.py
+++ src/config.py
@@ -12,15 +12,41 @@ class Configuration:
 
     def __init__(self):
 
-        # base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        self.base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
         '''
-        model parameters
+        Feature Extractor
         '''
+        self.input_frame_height = None
+        self.input_frame_width = None
+        self.input_frame_channels = None
+
+        self.conv_filters = None
+        self.conv_kernel_sizes  = None
+        self.conv_strides = None
+        self.conv_paddings = None
+        self.conv_activations = None
+        self.conv_initializer = None
+
+        self.fe_n_denses = None
+        self.fe_dense_activations = None
+        self.fe_dense_initializer = None
+        self.fe_n_outputs = None
 
         '''
-        training parameters
+        Agent
         '''
+        self.max_T = None
+        self.n_actions = None
+        self.linear_initializer = None
+        self.K_filters = None
+
+        self.ir_n_hidden = None
+        self.ir_activation = None
+        self.ir_initializer = None
+        self.n_epsilon_t = None
+
+        self.e = None
 
         '''
         log paths
