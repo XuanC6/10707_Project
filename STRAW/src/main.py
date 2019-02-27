829cf1e87dbdfa1d88bc860ef0a9e541988ce021
XuanC6 <95chenxuan@gmail.com>
2019-02-26 21:18:06 -0500

    707 project

---
+++ STRAW/src/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import time
+import numpy as np
+import sys, os, glob
+import tensorflow as tf
+
+from trainer import Trainer
+from config import Configuration
+
+'''
+Execute training
+'''
+
+if __name__ == "__main__":
+    pass
+
---
+++ STRAW/src/trainer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import sys
+import random
+
+from model import Agent_STRAWe
+
+'''
+Interact with the environment
+Define loss function and train_op
+'''
+
+class Trainer:
+    pass
